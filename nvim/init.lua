52da3a309557b8bffb349d16c9411b15f712818c
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-20 11:07:14 +0100

    :heavy_minus_sign: Remove Node and Deno lspconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-local nvim_lsp = require("lspconfig")
-nvim_lsp.tsserver.setup{
-  root_dir = nvim_lsp.util.root_pattern("package.json")
-}
-nvim_lsp.denols.setup {
-  root_dir = nvim_lsp.util.root_pattern("deno.json"),
-}
