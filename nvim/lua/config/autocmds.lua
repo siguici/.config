6d92901c7c2ee6765b0428fc94b834d1c821a061
⚡ Sigui <siguici@proton.me>
2025-04-29 16:16:04 +0100

    ⬆️ Upgrade dependencies

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,23 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
-
-local function save()
-  local buf = vim.api.nvim_get_current_buf()
-
-  vim.api.nvim_buf_call(buf, function()
-    vim.cmd("silent! write")
-  end)
-end
-
-vim.api.nvim_create_augroup("AutoSave", {
-  clear = true,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  callback = function()
-    save()
-  end,
-  pattern = "*",
-  group = "AutoSave",
-})
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
