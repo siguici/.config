a68b1b670e2ceda1ad4050a21ed0b75f08aa6190
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-05-07 10:44:27 +0100

    🐛 Fix Yanky on VSCode

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -2,13 +2,16 @@ return {
   {
     "gbprod/yanky.nvim",
     dependencies = not jit.os:find("Windows") and { "kkharji/sqlite.lua" } or {},
-    opts = {
-      highlight = { timer = 250 },
-      ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
-    },
+    config = function()
+      require("yanky").setup({
+        highlight = { timer = 250 },
+        ring = {
+          storage = jit.os:find("Windows") and "shada" or "sqlite",
+        },
+      })
+    end,
     keys = {
-      -- stylua: ignore
-    { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
       { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
       { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
       { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
