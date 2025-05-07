17425e7a15a38e03a9e582c3ae35f5dbd289a0da
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-05-07 10:53:05 +0100

    🐛 Fix Yanky
    Load Yanky first

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -2,14 +2,14 @@ return {
   {
     "gbprod/yanky.nvim",
     dependencies = not jit.os:find("Windows") and { "kkharji/sqlite.lua" } or {},
-    config = function()
-      require("yanky").setup({
-        highlight = { timer = 250 },
-        ring = {
-          storage = jit.os:find("Windows") and "shada" or "sqlite",
-        },
-      })
-    end,
+    lazy = false,
+    priority = 1001,
+    opts = {
+      highlight = { timer = 250 },
+      ring = {
+        storage = jit.os:find("Windows") and "shada" or "sqlite",
+      },
+    },
     keys = {
       { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
       { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
