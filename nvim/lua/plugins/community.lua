4f73be3425c62e2709ea3595d40f9d4e6367669f
Sigui Kessé Emmanuel <siguici@proton.me>
2026-09-20 01:07:12 +0000

    🔧 Add language tools

---
+++ nvim/lua/plugins/community.lua
@@ -0,0 +1,11 @@
+return {
+  -- Importation du dépôt de la communauté
+  "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+}
